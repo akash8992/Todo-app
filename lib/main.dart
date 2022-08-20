7c2b4490cb1bc02ed3319a4a43c2c37b179dc9c9
akash <akash8992@gmail.com>
2022-08-20 22:19:22 +0530

    todoo app home page and widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,23 @@
-// Copyright 2018 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'package:flutter/material.dart';
-import 'package:todoo_app/screens/homepage.dart';
+
+import 'screens/tasks_screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  
-  get image => null;
+  const MyApp({Key? key}) : super(key: key);
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home:Homepage(),
+      title: 'Flutter Tasks App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const TasksScreen(),
     );
   }
 }
